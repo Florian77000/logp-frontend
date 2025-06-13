a175b8051def54ec19c62741084967625c60c600
Florian <floriandelaunay75@gmail.com>
2025-06-13 19:00:55 +0200

    ajout du lien pour la page signin

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,30 @@
-    import styles from "../styles/Header.module.css";
-    import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-    import { faUser, faBars } from "@fortawesome/free-solid-svg-icons";
+import styles from "../styles/Header.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faBars } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
-    export default function Header() {
-    return (
-        <div className={styles.header}>
-        <div className={styles.logo}>
-            <img className={styles.logoImg} src="logo.png"></img>
-        </div>
-        <div className={styles.link}>
-            <li className={styles.linkText}>lien 1</li>
-            <li className={styles.linkText}>lien 2</li>
-            <li className={styles.linkText}>lien 3</li>
-        </div>
-        <div className={styles.icon}>
-            <FontAwesomeIcon className={styles.iconUser} icon={faUser} size="xl" />
-            <FontAwesomeIcon className={styles.iconBars} icon={faBars} size="2xl" />
-        </div>
-        </div>
-    );
-    }
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <img className={styles.logoImg} src="logo.png"></img>
+      </div>
+      <div className={styles.link}>
+        <li className={styles.linkText}>lien 1</li>
+        <li className={styles.linkText}>lien 2</li>
+        <li className={styles.linkText}>lien 3</li>
+      </div>
+      <div className={styles.icon}>
+        <Link href="/signin">
+          <FontAwesomeIcon
+            className={styles.iconUser}
+            icon={faUser}
+            size="xl"
+          />
+        </Link>
+
+        <FontAwesomeIcon className={styles.iconBars} icon={faBars} size="2xl" />
+      </div>
+    </div>
+  );
+}
---
+++ pages/signin.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+
+export default function Signin() {
+  return (
+    <div>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <h1>Se connecter</h1>
+      </main>
+    </div>
+  );
+}
