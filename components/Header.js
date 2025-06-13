ae80c85c9c833faaf32117f00394a1e1f91f3eeb
Florian <floriandelaunay75@gmail.com>
2025-06-13 17:17:42 +0200

    ajout de fonctionnalité sur le header : surlignement hover, ajout du logo, ajout de icon via fonteawesome

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,21 @@
 import styles from "../styles/Header.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return (
     <div className={styles.header}>
-      <div className={styles.logo}>logo</div>
+      <div className={styles.logo}>
+        <img className={styles.logoImg} src="logo.png"></img>
+      </div>
       <div className={styles.link}>
         <li className={styles.linkText}>lien 1</li>
-        <li>lien 2</li>
-        <li>lien 3</li>
+        <li className={styles.linkText}>lien 5</li>
+        <li className={styles.linkText}>lien 3</li>
       </div>
-      <div className={styles.icon}>icon connection</div>
+      <div className={styles.icon}>
+        <FontAwesomeIcon icon={faUser} size="xl" />
+      </div>
     </div>
   );
 }
