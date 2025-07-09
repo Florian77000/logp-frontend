e0458e956941b2757764c2acbe92ef6cef0c70fe
unknown <floriandelaunay75@gmail.com>
2025-07-09 17:18:44 +0200

    ajout des imput email et password pour l'inscription

--- pages/signin.js
+++ pages/signin.js
@@ -1,13 +1,31 @@
 import Header from "../components/Header";
+import { useState } from "react";
 
 export default function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () =>  {
+    fetch('http://localhost:3000/signin')
+    .then(response => response.json())
+ }
+  
   return (
     <div>
       <header>
         <Header />
       </header>
       <main>
-        <h1>Se connecter</h1>
+        <div>
+          <h1>Se connecter</h1>
+        </div>
+        <div>
+          <input type="email" placeholder="Votre email" onChange={(e) => setEmail(e.target.value)} value={email} />
+          <input type="password" placeholder="Votre mot de passe" onChange={(e) => setPassword(e.target.value)} value={password} />
+          <button onClick={() => handleSubmit()}>Login</button>
+        </div>
+        
+
       </main>
     </div>
   );
