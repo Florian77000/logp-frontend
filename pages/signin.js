dce288562cac4532b4b9f319c0f02e813a718bbf
unknown <floriandelaunay75@gmail.com>
2025-07-10 17:00:04 +0200

    ajout d'un message pour la creation du compte

--- pages/signin.js
+++ pages/signin.js
@@ -4,7 +4,8 @@ import { useState } from "react";
 export default function Signin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [message, setMessage] = useState("")
+ 
   //connexion sur la route pour ajouter un user
   const handleSubmit = () =>  {
     fetch('http://localhost:3000/users/new', {
@@ -15,8 +16,15 @@ export default function Signin() {
         password : password,
       }),
     })
-    .then((response) => response.json());
-    setEmail(""); //met le input email vide après click sur le bouton
+    .then((response) => response.json())
+    .then((data) => {
+      if(data.result === true) {
+        setMessage("ok")
+      } else {
+        setMessage("erreur")
+      }
+    })
+     setEmail(""); //met le input email vide après click sur le bouton
     setPassword(""); //met le input password vide après click sur le bouton
    }
   
@@ -33,6 +41,9 @@ export default function Signin() {
           <input type="email" placeholder="Votre email" onChange={(e) => setEmail(e.target.value)} value={email} />
           <input type="password" placeholder="Votre mot de passe" onChange={(e) => setPassword(e.target.value)} value={password} />
           <button onClick={() => handleSubmit()}>Login</button>
+        </div>
+        <div>
+          <p>{message}</p>
         </div>
         
 
